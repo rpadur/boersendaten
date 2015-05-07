059c4b5e8ddfca12a92a1e92f9ceb2339b3282d4
Ronald Padur <ronald.padur@dxmccw1.tolina.local>
2015-05-07 16:45:36 +0200

    rückbenennung

--- boersendaten/src/main/java/de/padur/boersendaten/Application.java
+++ boersendaten/src/main/java/de/padur/boersendaten/Application.java
@@ -4,9 +4,7 @@
 package de.padur.boersendaten;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
